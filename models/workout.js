80db203fee0a4bb5ba447e9b19c49b0087c7b943
marcustate <58437889+marcustate@users.noreply.github.com>
2020-04-23 18:44:05 -0500

    did some code on the workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,3 +1,40 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const workoutSchema = new schema (
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an excercise"
+                },
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Exercise type is Required"
+                },
+                duration: {
+                    type: "Exercise duration required in minutes"
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                weight: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
+)
